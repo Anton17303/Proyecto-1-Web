9edd5de36e6e2192bed7c62fde34f5325a6bf833
Anton17303 <105880067+Anton17303@users.noreply.github.com>
2025-05-25 23:56:30 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useCalculator } from './hooks/useCalculator'
-import Buton from './components/Buton'
+import Boton from './components/Boton'
 import Display from './components/Display'
 import './App.css'
 
@@ -18,25 +18,25 @@ const App = () => {
     <div className="contenedor">
       <div className="pad">
         <Display value={display} />
-        <Button text="Clear" onClick={clear} type="clear" />
-        <Button text="+/-" onClick={toggleSign} type="function" />
-        <Button text="%" onClick={() => performOperation('%')} type="operation" />
-        <Button text="7" onClick={() => inputNumber('7')} />
-        <Button text="8" onClick={() => inputNumber('8')} />
-        <Button text="9" onClick={() => inputNumber('9')} />
-        <Button text="/" onClick={() => performOperation('/')} type="operation" />
-        <Button text="4" onClick={() => inputNumber('4')} />
-        <Button text="5" onClick={() => inputNumber('5')} />
-        <Button text="6" onClick={() => inputNumber('6')} />
-        <Button text="*" onClick={() => performOperation('*')} type="operation" />
-        <Button text="1" onClick={() => inputNumber('1')} />
-        <Button text="2" onClick={() => inputNumber('2')} />
-        <Button text="3" onClick={() => inputNumber('3')} />
-        <Button text="-" onClick={() => performOperation('-')} type="operation" />
-        <Button text="0" onClick={() => inputNumber('0')} />
-        <Button text="." onClick={inputDecimal} />
-        <Button text="=" onClick={equals} type="equals" />
-        <Button text="+" onClick={() => performOperation('+')} type="operation" />
+        <Boton text="Clear" onClick={clear} type="clear" />
+        <Boton text="+/-" onClick={toggleSign} type="function" />
+        <Boton text="%" onClick={() => performOperation('%')} type="operation" />
+        <Boton text="7" onClick={() => inputNumber('7')} />
+        <Boton text="8" onClick={() => inputNumber('8')} />
+        <Boton text="9" onClick={() => inputNumber('9')} />
+        <Boton text="/" onClick={() => performOperation('/')} type="operation" />
+        <Boton text="4" onClick={() => inputNumber('4')} />
+        <Boton text="5" onClick={() => inputNumber('5')} />
+        <Boton text="6" onClick={() => inputNumber('6')} />
+        <Boton text="*" onClick={() => performOperation('*')} type="operation" />
+        <Boton text="1" onClick={() => inputNumber('1')} />
+        <Boton text="2" onClick={() => inputNumber('2')} />
+        <Boton text="3" onClick={() => inputNumber('3')} />
+        <Boton text="-" onClick={() => performOperation('-')} type="operation" />
+        <Boton text="0" onClick={() => inputNumber('0')} />
+        <Boton text="." onClick={inputDecimal} />
+        <Boton text="=" onClick={equals} type="equals" />
+        <Boton text="+" onClick={() => performOperation('+')} type="operation" />
       </div>
     </div>
   )
