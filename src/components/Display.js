db8638eadde652212f6c1d5e24e8d39042104ebc
Anton17303 <105880067+Anton17303@users.noreply.github.com>
2025-05-25 23:18:55 -0600

    Create Display.js

---
+++ src/components/Display.js
@@ -0,0 +1,5 @@
+const Display = ({ value }) => (
+  <div className="w-full h-12 bg-gray-100 border border-black rounded-lg flex items-center justify-end px-3 text-2xl font-mono mb-2">
+    {value}
+  </div>
+);
