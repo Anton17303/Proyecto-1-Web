9353823f303aa631607f6188c36ae7b23a4b091f
Anton17303 <105880067+Anton17303@users.noreply.github.com>
2025-05-25 23:24:40 -0600

    Update Boton.jsx

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,25 +1,26 @@
-const Button = ({ text, onClick, type = 'normal' }) => {
-  const getButtonClass = () => {
+import "../stylesheets/Boton.css"
+
+const Boton = ({ text, onClick, type = 'normal' }) => {
+  const getBotonClass = () => {
     switch (type) {
       case 'operation':
-        return 'bg-orange-500 hover:bg-orange-600';
+        return 'boton-operacion'
       case 'clear':
-        return 'bg-red-500 hover:bg-red-600 text-white col-span-2';
+        return 'boton-clear'
       case 'equals':
-        return 'bg-green-500 hover:bg-green-600 text-white';
+        return 'boton-equals'
       case 'function':
-        return 'bg-purple-500 hover:bg-purple-600 text-white';
+        return 'boton-function'
       default:
-        return 'bg-blue-400 hover:bg-blue-500';
+        return 'boton-normal'
     }
-  };
+  }
 
   return (
-    <button
-      className={`h-12 border border-black rounded-lg font-bold text-lg transition-colors ${getButtonClass()}`}
-      onClick={onClick}
-    >
+    <button className={getButtonClass()} onClick={onClick}>
       {text}
     </button>
-  );
-};
+  )
+}
+
+export default Boton
